b03e26e124cb8353bf75b8774337572fc091b1eb
spiderman <147817931+Iamsanji@users.noreply.github.com>
2024-11-29 11:56:40 +0800

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "styles/landing.css">
+    <title>OB Website</title>
+</head>
+<body>
+    
+    <header>
+        <nav class="header">
+            <nav class="title">
+                <h1>O<span>B</span></h1>
+            </nav>
+            
+            <nav class="menu">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="signin.php">Sign in</a>
+            </nav>
+            
+        </nav>
+    </header>
+
+    <main>
+
+        
+
+    </main>
+
+</body>
+</html>
